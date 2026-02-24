dceffe37319fea96186b1de3d1b3a2dbde3f4ca3
Nisharg Shah <nishargshah3101@gmail.com>
2026-02-25 03:51:40 +0530

    fix: lint

--- apps/web/src/app/providers.tsx
+++ apps/web/src/app/providers.tsx
@@ -5,7 +5,7 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 
-import ProgressBar from '@/components/loader/ProgressBar';
+import ProgressBar from '@/components/molecules/ProgressBar';
 import { useZustandState } from '@/hooks/useZustandState';
 import queryClient from '@/lib/queryClient';
 import ZustandProvider from '@/providers/ZustandProvider';
